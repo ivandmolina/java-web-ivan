9b586b20d3b2bce249c9b4129a9ac1109c712b74
xxx <xxx@xxxx>
2019-08-28 03:15:57 -0300

    Proyecto java web

---
+++ src/java/config/Conexion.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author xxx
+ */
+public class Conexion {
+    
+    Connection con;
+    String url= "jdbc:mysql://localhost:3306/bdcelulares";
+    String usuario="root";
+    String pass="";
+    
+    public Connection Conexion(){
+         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con= DriverManager.getConnection(url, usuario, pass);
+            
+        } catch (Exception e) {
+        }
+        return con;
+    }
+    
+}
---
+++ src/java/controlador/Controlador.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author xxx
+ */
+public class Controlador extends HttpServlet {
+
+    String usuarioDefecto = "ivan";
+    String contraDefecto = "12345";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String accion=request.getParameter("accion");
+        
+        switch(accion) {
+            case "Ingresar":
+                String nombreUsuario=request.getParameter("nombreUsuario");
+                String contra=request.getParameter("contra");
+                if(nombreUsuario.equals(usuarioDefecto) && contra.equals(contraDefecto)) {
+                    response.sendRedirect("PantallaPrincipal.jsp");
+                }
+                else {
+                    response.sendRedirect("index.jsp");
+                }
+                break;
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
